ab5361a5ca202830dcc7ff6ceca1f31266996f84
Jone Polvora <jpolvora@gmail.com>
2019-11-22 20:07:05 -0400

    update deploy

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,3 @@
-tar -cvf ./deploy.tar --exclude *.map ./captain-definition ./dist/*
-
+call npm run build 
+tar -cvf ./deploy.tar --exclude *.map ./captain-definition ./dist/**
 caprover deploy -t ./deploy.tar
