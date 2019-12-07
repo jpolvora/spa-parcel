4fdb63d55a1cd0943223f4c16dcd0a3d2f06eb01
Jone Pólvora <jpolvora@gmail.com>
2019-12-07 01:28:15 -0400

    update

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,3 @@
 call npm run build 
 tar -cvf ./deploy.tar --exclude *.map ./captain-definition ./dist/**
-caprover deploy -n caprover-jpolvora -a spa -t ./deploy.tar --default
+caprover deploy --default -t ./deploy.tar
